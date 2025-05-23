34cb86122b0b33d258270dff93462408dc4feda0
Muatasim-Aswad <malasoad2021@gmail.com>
2025-05-23 22:09:39 +0200

    auth - user

--- auth-service/src/main/java/com/asim/authentication/core/repository/UserRepository.java
+++ auth-service/src/main/java/com/asim/authentication/core/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package com.asim.authentication.core.repository;
 
-public interface UserRepository {
+import com.asim.authentication.core.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 }
--- auth-service/src/main/java/com/asim/authentication/core/service/UserService/UserServiceImpl.java
+++ auth-service/src/main/java/com/asim/authentication/core/service/UserService/UserServiceImpl.java
@@ -1,5 +1,12 @@
 package com.asim.authentication.core.service.UserService;
 
-public class UserServiceImpl {
+import com.asim.authentication.core.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
 
 }
