64c180a4dca1d583aad0d3536a1596fa222695a6
Muatasim-Aswad <malasoad2021@gmail.com>
2025-05-30 10:50:34 +0200

    add parent module and shared grpc module

---
+++ grpc-shared/src/main/java/com/asim/grpcshared/GrpcSharedApplication.java
@@ -0,0 +1,13 @@
+package com.asim.grpcshared;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcSharedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcSharedApplication.class, args);
+    }
+
+}
---
+++ grpc-shared/src/test/java/com/asim/grpcshared/GrpcSharedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asim.grpcshared;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcSharedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
